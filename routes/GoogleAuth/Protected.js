b0de15039ca830b4bd7a2cbb63d3a6a0c4ff28b8
Jamkris <dltmdgus1412@gmail.com>
2024-06-12 00:09:55 +0900

    ADD :: 미들웨어추가

--- routes/GoogleAuth/Protected.js
+++ routes/GoogleAuth/Protected.js
@@ -1,11 +1,11 @@
 const Protected = (req, res) => {
 	res.send(`
     <h1>Hello User!</h1>
-    <h3>UR Name : ${req.user.name}</h3>
-    <h3>UR Email : ${req.user.email}</h3>
-    <h3>UR GoogleID : ${req.user.userId}</h3>
-    <img src=${req.user.profileImg} alt="ProfileIMG" width='400' height='400'/>
-    <h3>Provider : ${req.user.provider}</h3>
+    <h3>UR Name : ${req.user?.name}</h3>
+    <h3>UR Email : ${req.user?.email}</h3>
+    <h3>UR GoogleID : ${req.user?.userId}</h3>
+    <img src=${req.user?.profileImg} alt="ProfileIMG" width='400' height='400'/>
+    <h3>Provider : ${req.user?.provider}</h3>
   `);
 };
 
