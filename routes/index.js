bffaecf448179fab821b533c0cd5913bccfc0825
Jamkris <dltmdgus1412@gmail.com>
2024-09-05 15:50:34 +0900

    ADD :: JWT 라우터 설정

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//간편로그인 라우터
+// 간편로그인 라우터
 const GoogleRouter = require('./GoogleAuth');
 router.use('/auth', GoogleRouter);
 const KakaoRouter = require('./KakaoAuth');
@@ -10,6 +10,10 @@ router.use('/auth', KakaoRouter);
 // JWT 인증 라우터
 const jwtRouter = require('./Jwt');
 router.use('/jwt', jwtRouter);
+
+// 새계정 생성 라우터
+const NewAccountSignUpRouter = require('./NewAccountSignUp')
+router.unsubscribe('/newaccount', NewAccountSignUpRouter)
 
 // 채팅 라우터
 const ChattingRouter = require('./Chatting');
