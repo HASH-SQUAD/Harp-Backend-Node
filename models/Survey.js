8ed3e72594180f58b90bb5e9efa9049f4b43136f
Jamkris <dltmdgus1412@gmail.com>
2024-09-09 10:27:35 +0900

    FIX :: Q3가 없어서 추가 및 컬럼타입 수정
    컬럼 타입이 JSON으로 되어있어 TEXT로 처리 및 Q3 컬럼 추가함

--- models/Survey.js
+++ models/Survey.js
@@ -9,30 +9,14 @@ module.exports = (sequelize, DataTypes) => {
 			},
 			question1: {
 				type: DataTypes.TEXT,
-				get: function () {
-					try {
-						return JSON.parse(this.getDataValue('question1'));
-					} catch (error) {
-						return this.getDataValue('question1');
-					}
-				},
-				set: function (value) {
-					this.setDataValue('question1', JSON.stringify(value));
-				},
 				allowNull: true,
 			},
 			question2: {
 				type: DataTypes.TEXT,
-				get: function () {
-					try {
-						return JSON.parse(this.getDataValue('question2'));
-					} catch (error) {
-						return this.getDataValue('question2');
-					}
-				},
-				set: function (value) {
-					this.setDataValue('question2', JSON.stringify(value));
-				},
+				allowNull: true,
+			},
+			question3: {
+				type: DataTypes.TEXT,
 				allowNull: true,
 			},
 			etc: {
