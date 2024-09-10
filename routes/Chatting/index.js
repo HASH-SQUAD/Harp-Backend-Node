b0fd14b7fa4a1aabde42e11be2a66d2018257a35
Jamkris <dltmdgus1412@gmail.com>
2024-09-10 21:55:33 +0900

    ADD :: 데이트 채팅 가능

--- routes/Chatting/index.js
+++ routes/Chatting/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const { validateToken } = require('../../middlewares/AuthMiddleware.js');
 
-const requestChat = require('../Chatting/TravelChat.js');
-router.post('/travel/:id', validateToken, requestChat);
-
 const createAI = require('../Chatting/AI.js');
 router.post('/createai', validateToken, createAI);
+
+const TravelChat = require('../Chatting/TravelChat.js');
+router.post('/travel/:id', validateToken, TravelChat);
+
+const DateChat = require('../Chatting/DateChat.js');
+router.post('/date/:id', validateToken, DateChat);
 
 module.exports = router;
