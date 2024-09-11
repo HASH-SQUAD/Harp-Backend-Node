ff690299912094c8527ec88a8e303073e7615856
Jamkris <dltmdgus1412@gmail.com>
2024-09-12 00:02:35 +0900

    ADD :: 로그세팅

--- index.js
+++ index.js
@@ -4,33 +4,40 @@ const dotenv = require('dotenv');
 dotenv.config();
 const path = require('path');
 const authUtil = require('./response/authUtil');
+const logger = require('./logger');
+const requestIp = require('request-ip');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname + '/public')));
 
-//Port Setting
+// Server Logs
+app.use((req, res, next) => {
+    const ipAddress = requestIp.getClientIp(req);
+    const requestPath = req.path;
+    const method = req.method;
+    logger.info(`Request received`, { ip: ipAddress, path: requestPath, method: method });
+    next();
+});
+
+// Port Setting
 const PORT = process.env.PORT;
 
-//DataBase
+// DataBase
 const db = require('./models');
 
-//Server Test
+// Server Test
 app.get('/', (req, res) => {
     res.send(`HARP Server is Running Port ${process.env.PORT}`);
 });
 
-//404 NotFound
-app.get('*', (req, res) => {
-    const requestPath = req.path;
-    return res.status(404).send(authUtil.successFalse(404, "라우팅을 찾을 수 없습니다.", { path: requestPath }))
-})
-
-//API Router Call
+// API Router Call
 const ApiRouter = require('./routes/');
 app.use('/', ApiRouter);
 
-//Port
+// Port
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    app.listen(PORT, () => {
+        logger.info(`Sever Started on Port ${process.env.PORT}`);
+    });
 });
