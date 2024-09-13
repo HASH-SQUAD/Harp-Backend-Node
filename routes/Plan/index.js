6dce9b8e35817638e9c33b34a9b3d1bb97ea1848
Jamkris <dltmdgus1412@gmail.com>
2024-09-14 01:02:19 +0900

    ADD :: 모든 일정 가져오기

--- routes/Plan/index.js
+++ routes/Plan/index.js
@@ -6,4 +6,7 @@ const { validateToken } = require('../../middlewares/AuthMiddleware.js');
 const CreatePlan = require('./CreatePlan.js');
 router.post('/', validateToken, CreatePlan);
 
+const GetPlan = require('./GetPlanAll.js')
+router.get('/', validateToken, GetPlan)
+
 module.exports = router;
