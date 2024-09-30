ae2034f0898cedb77108ec476e0540b2cc89e15e
Jamkris <dltmdgus1412@gmail.com>
2024-09-30 17:29:13 +0900

    FIX :: AI 프롬프트 수정
    프롬픝 수정 및 Divide를 넣어 Travel&Date를 구분 그리고 문화시설과 관광명소 여행일 경우 숙박업소도 데이터를 추가했음

--- routes/Chatting/DateChat.js
+++ routes/Chatting/DateChat.js
@@ -11,6 +11,8 @@ const DateChat = async (req, res) => {
   const userId = req.user.dataValues.userId;
   const aiId = req.params.id;
 
+  await AI.update({ locationData: location, divide: "Date" }, { where: { aiId: aiId } });
+
   try {
     let aiRecord = await AI.findOne({ where: { aiId: aiId } });
 
@@ -29,6 +31,7 @@ const DateChat = async (req, res) => {
     let location_Response = {};
 
     if (location) {
+      //카페
       const cafeResponse = await axios({
         method: 'GET',
         url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=FD6`,
@@ -36,6 +39,7 @@ const DateChat = async (req, res) => {
           'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
         }
       });
+      //음식점
       const foodResponse = await axios({
         method: 'GET',
         url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=CE7`,
@@ -43,10 +47,29 @@ const DateChat = async (req, res) => {
           'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
         }
       });
+      //문화시설
+      const CulturalResponse = await axios({
+        method: 'GET',
+        url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=CT1`,
+        headers: {
+          'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
+        }
+      });
+      //관광명소
+      const TouristResponse = await axios({
+        method: 'GET',
+        url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=AT4`,
+        headers: {
+          'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
+        }
+      });
+
 
       location_Response = {
         cafe: cafeResponse.data,
-        food: foodResponse.data
+        food: foodResponse.data,
+        cultur: CulturalResponse.data,
+        tour: TouristResponse.data
       };
     }
 
@@ -60,7 +83,7 @@ const DateChat = async (req, res) => {
       const systemMessage = previousConversations.messages[systemMessageIndex];
 
       const updatedContent = `
-        카페&음식점 정보: ${JSON.stringify(location_Response)}
+        카페&음식점&문화시설&관광명소 정보: ${JSON.stringify(location_Response)}
 				사용자가 좋아하는 여행 유형: ${UserSurveyData.dataValues.question1}
 				사용자의 음식 취향: ${UserSurveyData.dataValues.question2}
 				사용자의 MBTI: ${UserSurveyData.dataValues.question3}
@@ -73,7 +96,7 @@ const DateChat = async (req, res) => {
       previousConversations.messages.unshift({
         role: 'system',
         content: `
-          카페&음식점 정보: ${JSON.stringify(location_Response)}
+          카페&음식점&문화시설&관광명소 정보: ${JSON.stringify(location_Response)}
           시스템 데이터: ${JSON.stringify(Conversation, null, 2)}
           사용자가 좋아하는 여행 유형: ${UserSurveyData.dataValues.question1}
           사용자의 음식 취향: ${UserSurveyData.dataValues.question2}
@@ -98,7 +121,7 @@ const DateChat = async (req, res) => {
       data: {
         model: 'gpt-4o-mini',
         messages: previousConversations.messages,
-        max_tokens: 8000,
+        max_tokens: 15000,
         temperature: 0.40,
         top_p: 1,
         frequency_penalty: 0,
--- routes/Chatting/TravelChat.js
+++ routes/Chatting/TravelChat.js
@@ -12,6 +12,8 @@ const TravelChat = async (req, res) => {
 	const userId = req.user.dataValues.userId;
 	const aiId = req.params.id;
 
+	await AI.update({ locationData: location, divide: "Travel" }, { where: { aiId: aiId } });
+
 	try {
 		let aiRecord = await AI.findOne({ where: { aiId: aiId } });
 
@@ -30,6 +32,7 @@ const TravelChat = async (req, res) => {
 		let location_Response = {};
 
 		if (location) {
+			//카페
 			const cafeResponse = await axios({
 				method: 'GET',
 				url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=FD6`,
@@ -37,6 +40,7 @@ const TravelChat = async (req, res) => {
 					'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
 				}
 			});
+			//음식점
 			const foodResponse = await axios({
 				method: 'GET',
 				url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=CE7`,
@@ -44,10 +48,37 @@ const TravelChat = async (req, res) => {
 					'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
 				}
 			});
+			//문화시설
+			const CulturalResponse = await axios({
+				method: 'GET',
+				url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=CT1`,
+				headers: {
+					'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
+				}
+			});
+			//관광명소
+			const TouristResponse = await axios({
+				method: 'GET',
+				url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=AT4`,
+				headers: {
+					'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
+				}
+			});
+			//숙박업소
+			const LodgmentResponse = await axios({
+				method: 'GET',
+				url: `https://dapi.kakao.com/v2/local/search/keyword?query=${location}&page=1&size=10&category_group_code=AD5`,
+				headers: {
+					'Authorization': `KakaoAK ${process.env.KAKAO_CLIENT_ID}`
+				}
+			});
 
 			location_Response = {
 				cafe: cafeResponse.data,
-				food: foodResponse.data
+				food: foodResponse.data,
+				cultur: CulturalResponse.data,
+				tour: TouristResponse.data,
+				lodgment: LodgmentResponse.data
 			};
 		}
 
@@ -61,7 +92,7 @@ const TravelChat = async (req, res) => {
 			const systemMessage = previousConversations.messages[systemMessageIndex];
 
 			const updatedContent = `
-        카페&음식점 정보: ${JSON.stringify(location_Response)}
+        카페&음식점&문화시설&관광명소&숙박업소 정보: ${JSON.stringify(location_Response)}
 				사용자가 좋아하는 여행 유형: ${UserSurveyData.dataValues.question1}
 				사용자의 음식 취향: ${UserSurveyData.dataValues.question2}
 				사용자의 MBTI: ${UserSurveyData.dataValues.question3}
@@ -74,7 +105,7 @@ const TravelChat = async (req, res) => {
 			previousConversations.messages.unshift({
 				role: 'system',
 				content: `
-          카페&음식점 정보: ${JSON.stringify(location_Response)}
+					카페&음식점&문화시설&관광명소&숙박업소 정보: ${JSON.stringify(location_Response)}
           시스템 데이터: ${JSON.stringify(Conversation, null, 2)}
 					사용자가 좋아하는 여행 유형: ${UserSurveyData.dataValues.question1}
 					사용자의 음식 취향: ${UserSurveyData.dataValues.question2}
@@ -99,7 +130,7 @@ const TravelChat = async (req, res) => {
 			data: {
 				model: "gpt-4o-mini",
 				messages: previousConversations.messages,
-				max_tokens: 8000,
+				max_tokens: 15000,
 				temperature: 0.40,
 				top_p: 1,
 				frequency_penalty: 0,
