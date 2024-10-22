4939ba45a9867a2c57e88d8dc6e695dba164d18d
Jamkris <dltmdgus1412@gmail.com>
2024-10-23 00:18:24 +0900

    ADD :: 커뮤니티 model정의

---
+++ models/Community.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  const Community = sequelize.define(
+    'Community',
+    {
+      communityId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      des: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'userId',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    },
+    {
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    }
+  );
+
+  Community.associate = models => {
+    Community.belongsTo(models.Users, { foreignKey: 'userId', targetKey: 'userId' });
+  };
+
+  return Community;
+};
