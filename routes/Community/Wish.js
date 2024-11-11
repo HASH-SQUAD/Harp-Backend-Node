aeaaab284a7c17fa4550421a399cd909c4d60fdc
Jamkris <dltmdgus1412@gmail.com>
2024-11-11 23:01:31 +0900

    FIX :: 찜에 deletedAt이 있어 삭제 후 찜이 추가 되지 않는 문제

--- routes/Community/Wish.js
+++ routes/Community/Wish.js
@@ -1,23 +1,32 @@
 const authUtil = require('../../response/authUtil.js');
 const { Wish } = require('../../models');
+const { Op } = require('sequelize');
 
 const ToggleWish = async (req, res) => {
   const postId = req.params.id;
   const userId = req.user.dataValues.userId;
 
   try {
-    const existingWish = await Wish.findOne({
+    const wish = await Wish.findOne({
       where: {
         userId: userId,
-        communityId: postId
-      }
+        communityId: postId,
+      },
+      paranoid: false
     });
 
-    if (existingWish) {
-      await existingWish.destroy();
-      return res
-        .status(200)
-        .send(authUtil.successTrue(200, '찜 취소 성공'));
+    if (wish) {
+      if (wish.deletedAt === null) {
+        await wish.destroy();
+        return res
+          .status(200)
+          .send(authUtil.successTrue(200, '찜 취소 성공'));
+      } else {
+        await wish.restore();
+        return res
+          .status(200)
+          .send(authUtil.successTrue(200, '찜 추가 성공'));
+      }
     } else {
       await Wish.create({
         userId: userId,
@@ -29,6 +38,11 @@ const ToggleWish = async (req, res) => {
     }
   } catch (error) {
     console.error(error);
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      return res
+        .status(400)
+        .send(authUtil.successFalse(400, '이미 찜한 게시물입니다'));
+    }
     return res.status(500).send(authUtil.unknownError({ error: error.message }));
   }
 };
